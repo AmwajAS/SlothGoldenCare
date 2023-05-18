a7f345a7839caf94f3e205b1e589b5012e96a342
AmwajAS <amwaj.a.s@gmail.com>
2023-05-18 13:36:36 +0300

    First Activity - MainActivity with Splash screen.

--- app/src/main/java/com/example/slothgoldencare/MainActivity.java
+++ app/src/main/java/com/example/slothgoldencare/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.slothgoldencare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+
+        image = findViewById(R.id.image);
+        logo = findViewById(R.id.logo);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+
+
     }
 }
