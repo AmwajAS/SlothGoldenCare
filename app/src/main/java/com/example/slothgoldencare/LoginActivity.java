7d148cf19e54571e7d9cf1f71d3d39f35357f525
AmwajAS <amwaj.a.s@gmail.com>
2023-06-05 19:34:29 +0300

    Updated Login.

--- app/src/main/java/com/example/slothgoldencare/LoginActivity.java
+++ app/src/main/java/com/example/slothgoldencare/LoginActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,9 +14,6 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
 
     private EditText userid;
-    private Button loginBtn;
-    private Button elderBtn;
-    private Button userBtn;
     DataBaseHelper dbHelper;
 
 
@@ -26,95 +22,68 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
-        elderBtn =(Button) findViewById(R.id.elderBtn);
-        userBtn = (Button) findViewById(R.id.userBtn);
+        Button elderBtn = (Button) findViewById(R.id.elderBtn);
+        Button userBtn = (Button) findViewById(R.id.userBtn);
         userid = (EditText) findViewById(R.id.userid);
-        loginBtn = (Button) findViewById(R.id.loginBtn);
+        Button loginBtn = (Button) findViewById(R.id.loginBtn);
         dbHelper = new DataBaseHelper(this);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String uid = userid.getText().toString();
-                if(uid.equals("")){
-                    Toast.makeText(LoginActivity.this, "Please enter your ID", Toast.LENGTH_SHORT).show();
-                    Log.i(TAG, "This is a debug message " + uid); // Debug log
 
-                }else{
-                    Boolean checkUID =dbHelper.checkUserID(uid);
-                    if(checkUID){
+        loginBtn.setOnClickListener(v -> {
+            String uid = userid.getText().toString();
+            if (uid.equals("")) { //check if the id number / field is empty.
+                Toast.makeText(LoginActivity.this, "Please enter your ID", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "This is a debug message " + uid); // Debug log
+
+            } else {
+             //   if (checkIDValidation(uid)) {
+                    if (dbHelper.checkUserID(uid)) {
                         Log.i(TAG, "This is a debug message " + " Login Successfully"); // Debug log
-
                         Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(getApplicationContext(), HomePageActivity.class);
                         startActivity(intent);
-                    }
-                    else{
+                    } else {
                         Log.i(TAG, "This is a debug message " + " Failed to Login"); // Debug log
 
                         Toast.makeText(LoginActivity.this, "Failed to Login", Toast.LENGTH_SHORT).show();
 
                     }
-                }
+                //}
             }
         });
 
 
-
-        elderBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ElderSignupActivity.class);
-                startActivity(intent);
-            }
+        elderBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), ElderSignupActivity.class);
+            startActivity(intent);
         });
-        userBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), UserSignupActivity.class);
-                startActivity(intent);
-            }
+        userBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), UserSignupActivity.class);
+            startActivity(intent);
         });
-
-
-
 
 
     }
-//    public void checkID() {
-//        loginBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // Perform action when button is clicked
-//                // Create an Intent to start the new activity
-//                String uid = userid.getText().toString();
-//
-//                if (uid.length() == 0 ) {
-//                    toastMessage("Please fill ID field.");
-//                    Log.i(TAG, "This is a debug message"); // Debug log
-//
-//                } else {
-//                    Log.i(TAG, "This is a debug message "+ uid);
-//                    Boolean checkUID = dbHelper.checkUserID(uid);
-//                    Log.i(TAG, "This is a debug message"+ checkUID); // Debug log
-//
-//                    if (checkUID) {
-//                        Intent intent = new Intent(LoginActivity.this, HomePageActivity.class);
-//                        startActivity(intent);
-//                    } else {
-//                        toastMessage("Something Went Wrong - ID doesn't exist");
-//                    }
-//                }
-//
-//            }
-//        });
-//
-//
-//
+
+    /*
+    Since the Real Id Number consists of 9 digits Only, So in this method we check the Id Validation.
+    9 numbers only between 0-9.
+     */
+    public boolean checkIDValidation(String idV) {
+        return idV.matches("[0-9]{9}");
+    }
+
+
+//    public int checkUserType(User user){
+//        if(user instanceof Elder){
+//            return 1;
+//        }else {
+//            return 0;
+//        }
 //    }
 
 
-    private void toastMessage(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
+//    private void toastMessage(String message) {
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//    }
 
 }
