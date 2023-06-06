0c98060803c7c1a93be3f784d91bd77bffba694f
AmwajAS <amwaj.a.s@gmail.com>
2023-06-06 17:26:33 +0300

    user homepage activity + action bar (still on work)

---
+++ app/src/main/java/com/example/slothgoldencare/UserHomePageActivity.java
@@ -0,0 +1,33 @@
+package com.example.slothgoldencare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class UserHomePageActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home_page);
+        toolbar = findViewById(R.id.actBar);
+
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu, menu);
+        return true;
+    }
+
+
+}
