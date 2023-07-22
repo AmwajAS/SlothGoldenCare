ac9414e7f57408dc364a0ce5930fc2c6ee7a8df2
habeb <habeb.naw@outlook.com>
2023-07-22 15:42:55 +0300

    Relatives can now see elderly's data(diagnosis,allergies,surgeries).

---
+++ app/src/main/java/com/example/slothgoldencare/HealthStatusRelativeFragment.java
@@ -0,0 +1,164 @@
+package com.example.slothgoldencare;
+
+import android.os.Bundle;
+
+import android.widget.*;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.slothgoldencare.Model.Allergy;
+import com.example.slothgoldencare.Model.Diagnosis;
+import com.example.slothgoldencare.Model.Surgery;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HealthStatusFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HealthStatusRelativeFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private ListView list;
+    private List<Diagnosis> diagnosisList;
+    private List<Surgery> surgeryList;
+    private List<Allergy> allergyList;
+    private TextView list_title;
+    private Button backBtn;
+
+
+    private ImageView imageViewHealth;
+
+    public HealthStatusRelativeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HealthStatusRelativeFragment newInstance(Bundle args) {
+        HealthStatusRelativeFragment fragment = new HealthStatusRelativeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_health_status, container, false);
+        list = view.findViewById(R.id.health_status_list);
+        list_title = view.findViewById(R.id.list_title);
+
+        // back btn to remove the replaced view to the main one.
+        backBtn = view.findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(view1 -> {
+            requireActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
+            requireActivity().getSupportFragmentManager().popBackStack();
+        });
+
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(this.getContext());
+        Bundle args = getArguments();
+        if(args != null){
+            String value = args.getString("Button");
+            String elderlyDocId = args.getString("elderlyDocId");
+            if(value.equals("diagnosis")){
+                list_title.setText(R.string.diagnosis);
+                diagnosisList = dataBaseHelper.getDiagnosisByElderlyDocId(elderlyDocId);
+                ArrayAdapter<Diagnosis> diagnosisAdapter = new ArrayAdapter<Diagnosis>(this.getContext(), R.layout.health_status_item_layout, diagnosisList) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+                        // Get the user object for the current position
+                        Diagnosis diagnosis = getItem(position);
+
+                        // Inflate the list item layout
+                        if (convertView == null) {
+                            convertView = LayoutInflater.from(getContext()).inflate(R.layout.health_status_item_layout, parent, false);
+                        }
+                        TextView idText = convertView.findViewById(R.id.health_status_item);
+                        idText.setText(diagnosis.getDiagnosis());
+
+                        return convertView;
+                    }
+                };
+                list.setAdapter(diagnosisAdapter);
+
+            }else if(value.equals("allergies")){
+                list_title.setText(R.string.allergies);
+                allergyList = dataBaseHelper.getAllergiesByElderlyDocId(elderlyDocId);
+
+                ArrayAdapter<Allergy> allergyAdapter = new ArrayAdapter<Allergy>(this.getContext(), R.layout.health_status_item_layout, allergyList) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+                        // Get the user object for the current position
+                        Allergy allergy = getItem(position);
+
+                        // Inflate the list item layout
+                        if (convertView == null) {
+                            convertView = LayoutInflater.from(getContext()).inflate(R.layout.health_status_item_layout, parent, false);
+                        }
+                        TextView idText = convertView.findViewById(R.id.health_status_item);
+                        idText.setText(allergy.getAllergy());
+
+                        return convertView;
+                    }
+                };
+                list.setAdapter(allergyAdapter);
+
+            }else if(value.equals("surgeries")){
+                list_title.setText(R.string.surgery);
+                surgeryList = dataBaseHelper.getSurgeriesByElderlyDocId(elderlyDocId);
+
+                ArrayAdapter<Surgery> surgeryAdapter = new ArrayAdapter<Surgery>(this.getContext(), R.layout.surgery_item_layout, surgeryList) {
+                    @NonNull
+                    @Override
+                    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+                        // Get the user object for the current position
+                        Surgery surgery = getItem(position);
+
+                        // Inflate the list item layout
+                        if (convertView == null) {
+                            convertView = LayoutInflater.from(getContext()).inflate(R.layout.surgery_item_layout, parent, false);
+                        }
+                        TextView idText = convertView.findViewById(R.id.surgery_list_item);
+                        TextView dateText = convertView.findViewById(R.id.surgery_list_date);
+                        idText.setText(surgery.getSurgery());
+                        dateText.setText(surgery.getDate().toString());
+
+                        return convertView;
+                    }
+                };
+                list.setAdapter(surgeryAdapter);
+            }
+        }
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/slothgoldencare/VisitElderlyProfileActivity.java
+++ app/src/main/java/com/example/slothgoldencare/VisitElderlyProfileActivity.java
@@ -3,10 +3,14 @@ package com.example.slothgoldencare;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.example.slothgoldencare.Model.Elder;
 
 public class VisitElderlyProfileActivity  extends AppCompatActivity implements View.OnClickListener {
@@ -15,6 +19,8 @@ public class VisitElderlyProfileActivity  extends AppCompatActivity implements V
     private DataBaseHelper dataBaseHelper;
     private TextView editTextUsername;
     private CardView D1, D2, D3, D4, D5, D6;
+    private  Elder elder;
+    private Button backBtn;
 
 
     @Override
@@ -25,7 +31,11 @@ public class VisitElderlyProfileActivity  extends AppCompatActivity implements V
         //statring variables
         elderlyId = getIntent().getStringExtra("elderlyId");
         dataBaseHelper = new DataBaseHelper(this);
-        Elder elder = dataBaseHelper.getElderById(elderlyId);
+        elder = dataBaseHelper.getElderById(elderlyId);
+        backBtn = findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(view -> {
+            this.finish();
+        });
 
         //edit layout
         editTextUsername = findViewById(R.id.username);
@@ -48,23 +58,45 @@ public class VisitElderlyProfileActivity  extends AppCompatActivity implements V
     }
 
     @Override
+
     public void onClick(View v) {
+        HealthStatusRelativeFragment healthStatusFragment;
         Intent i;
         switch (v.getId()) {
             case R.id.d1:
-                i = new Intent(this, MedicalServices.class);
-                startActivity(i);
+                Bundle args1 = new Bundle();
+                replaceFragment(new DoctorsFragment());
                 break;
-            case R.id.d5:
-                break;
-            case R.id.d6:
-                i = new Intent(this, SettingsActivity.class);
-                startActivity(i);
+            case R.id.d2:
                 break;
             case R.id.d4:
-                i = new Intent(this, ContactsActivity.class);
-                startActivity(i);
+                Bundle args4 = new Bundle();
+                args4.putString("Button","allergies");
+                args4.putString("elderlyDocId",elder.getDocId());
+                healthStatusFragment= HealthStatusRelativeFragment.newInstance(args4);
+                replaceFragment(healthStatusFragment);
                 break;
+            case R.id.d5:
+                Bundle args5 = new Bundle();
+                args5.putString("Button","diagnosis");
+                args5.putString("elderlyDocId",elder.getDocId());
+                healthStatusFragment= HealthStatusRelativeFragment.newInstance(args5);
+                replaceFragment(healthStatusFragment);
+                break;
+            case R.id.d6:
+                Bundle args6 = new Bundle();
+                args6.putString("Button","surgeries");
+                args6.putString("elderlyDocId",elder.getDocId());
+                healthStatusFragment= HealthStatusRelativeFragment.newInstance(args6);
+                replaceFragment(healthStatusFragment);
+                break;
+
         }
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.MedicalServicesFrameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
