c29e84efd4152171cb8ccceaafc3eeb4acbffa50
arwadsa <117629832+arwadsa@users.noreply.github.com>
2023-07-28 18:30:36 +0300

    Adding checks and alerts.

--- app/src/main/java/com/example/slothgoldencare/AppointmentSchedulingActivity.java
+++ app/src/main/java/com/example/slothgoldencare/AppointmentSchedulingActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-    public class AppointmentSchedulingActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AppointmentSchedulingActivity extends AppCompatActivity {
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +20,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
             DatePicker datePicker = findViewById(R.id.datePicker);
             Button nextDateButton = findViewById(R.id.nextButton1);
-
             nextDateButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     // Get the selected date from DatePicker
                     int year = datePicker.getYear();
-                    int month = datePicker.getMonth() + 1; // Month is 0-based
+                    int month = datePicker.getMonth();
                     int day = datePicker.getDayOfMonth();
 
-                    // Start the next activity (TimeActivity) and pass the selected date and time as a Timestamp extra
-                    Intent intent = new Intent(AppointmentSchedulingActivity.this, TimeActivity.class);
-                    intent.putExtra("selectedDate", year + "-" + month + "-" + day);
-                    startActivity(intent);
+                    // Create a Calendar instance for the selected date
+                    Calendar selectedDateCalendar = Calendar.getInstance();
+                    selectedDateCalendar.set(year, month, day);
+
+                    // Get the current date and time
+                    Calendar currentDateCalendar = Calendar.getInstance();
+
+                    // Check if the selected date is before the current date
+                    if (selectedDateCalendar.before(currentDateCalendar)) {
+                        // Show a toast message to pick the right date
+                        Toast.makeText(AppointmentSchedulingActivity.this, "Please select a date that is after the current date", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Start the next activity (TimeActivity) and pass the selected date and time as a Timestamp extra
+                        Intent intent = new Intent(AppointmentSchedulingActivity.this, TimeActivity.class);
+                        intent.putExtra("selectedDate", year + "-" + (month + 1) + "-" + day);
+                        startActivity(intent);
+                    }
                 }
             });
         }
--- app/src/main/java/com/example/slothgoldencare/DoctorsNotesActivity.java
+++ app/src/main/java/com/example/slothgoldencare/DoctorsNotesActivity.java
@@ -1,5 +1,6 @@
 package com.example.slothgoldencare;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.slothgoldencare.Model.Appointment;
@@ -44,14 +46,24 @@ public class DoctorsNotesActivity extends AppCompatActivity {
         notesEditText = findViewById(R.id.notesEditText);
         idText = findViewById(R.id.textViewId);
         scheduleButton = findViewById(R.id.scheduleButton);
-
         scheduleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String selectedDoctor = doctorsSpinner.getSelectedItem().toString();
-                String elderId= idText.getText().toString();
+                String elderId = idText.getText().toString();
                 String notes = notesEditText.getText().toString();
-                saveAppointmentData(selectedDateTime, elderId, notes, selectedDoctor);
+//                checks
+                if (selectedDoctor.isEmpty()) {
+                    Toast.makeText(DoctorsNotesActivity.this, "Please select a doctor", Toast.LENGTH_SHORT).show();
+                }
+                else if (elderId.isEmpty()) {
+                    Toast.makeText(DoctorsNotesActivity.this, "Please enter your ID", Toast.LENGTH_SHORT).show();
+                }
+                else if (!elderId.matches("\\d{9}")) {
+                    Toast.makeText(DoctorsNotesActivity.this, "Please enter a valid 9-digit ID", Toast.LENGTH_SHORT).show();
+                } else {
+                    saveAppointmentData(selectedDateTime, elderId, notes, selectedDoctor);
+                }
             }
         });
     }
@@ -66,25 +78,36 @@ public class DoctorsNotesActivity extends AppCompatActivity {
 
     private void saveAppointmentData(Timestamp dateTime, String elder,  String notes, String doctor) {
         db = FirebaseFirestore.getInstance();
-
-        // Create a new appointment object
         Appointment appointment = new Appointment(selectedDateTime, elder, notes, doctor);
-
-        // Add the appointment to the "appointments" collection in Firestore
         db.collection("Appointment")
                 .add(appointment)
                 .addOnSuccessListener(documentReference -> {
                     // The appointment was successfully added to Firestore
-                    // You can perform any further actions or show a success message here
                     Toast.makeText(this, "Appointment scheduled successfully!", Toast.LENGTH_SHORT).show();
-//                    showAppointmentConfirmation();
-                    finish();
+                    showSuccessAlert();
                 })
                 .addOnFailureListener(e -> {
                     // Failed to add the appointment to Firestore
                     Toast.makeText(this, "Failed to schedule appointment. Please try again later.", Toast.LENGTH_SHORT).show();
                     Log.e("DoctorsNotesActivity", "Error saving appointment data: " + e.getMessage());
-                    // You can show an error message here or handle the failure in any other way
                 });
+    }
+
+    private void showSuccessAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Success");
+        builder.setMessage("Appointment scheduled successfully!");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // The user clicked "OK," so dismiss the dialog
+                dialog.dismiss();
+                // Navigate to the desired activity using an Intent
+                Intent intent = new Intent(DoctorsNotesActivity.this, HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
