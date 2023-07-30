4265711748367b0de2585402160056dbf8a88871
AmwajAS <amwaj.a.s@gmail.com>
2023-07-30 23:18:26 +0300

    Search bar at the Elderlies list for the Doctor

--- app/src/main/java/com/example/slothgoldencare/DoctorActivity.java
+++ app/src/main/java/com/example/slothgoldencare/DoctorActivity.java
@@ -19,15 +19,14 @@ import java.util.List;
 
 public class DoctorActivity extends AppCompatActivity{
     private static final String TAG = "ADMIN";
-
     private List<Elder> elderlies;
     private ListView elderliesList;
-    private SearchView searchView;
-    private TextView editTextUsername;
+
     private String username;
     private FirebaseAuth auth;
     private DataBaseHelper dbHelper;
     private String userId;
+    private SearchView searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +34,14 @@ public class DoctorActivity extends AppCompatActivity{
         setContentView(R.layout.activity_doctor);
 
         auth = FirebaseAuth.getInstance();
-        editTextUsername = findViewById(R.id.editTextUsername);
+        searchBar = findViewById(R.id.searchBar);
         username = getIntent().getStringExtra("username");
         userId = getIntent().getStringExtra("userID");
-
-        editTextUsername.setText(auth.getCurrentUser().getDisplayName());
 
         dbHelper = new DataBaseHelper(this);
         elderlies = new ArrayList<>();
         elderliesList = findViewById(R.id.elderly_list);
         elderlies = dbHelper.getElders();
-
 
 
         ArrayAdapter<Elder> elderAdapter = new ArrayAdapter<Elder>(this, R.layout.doctor_elderly_item, elderlies) {
@@ -67,8 +63,8 @@ public class DoctorActivity extends AppCompatActivity{
                 convertView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Intent intent = new Intent(getApplicationContext(),DoctorVisitElderlyActivity.class);
-                        intent.putExtra("elderlyId",elder.getID());
+                        Intent intent = new Intent(getApplicationContext(), DoctorVisitElderlyActivity.class);
+                        intent.putExtra("elderlyId", elder.getID());
                         startActivity(intent);
                         Toast.makeText(getContext(), "Elder ID: " + elder.getID() + " is clicked.", Toast.LENGTH_SHORT).show();
                     }
@@ -76,8 +72,79 @@ public class DoctorActivity extends AppCompatActivity{
 
                 return convertView;
             }
+
+            @NonNull
+            @Override
+            public Filter getFilter() {
+                Filter filter = new Filter() {
+                    @Override
+                    protected FilterResults performFiltering(CharSequence constraint) {
+                        FilterResults results = new FilterResults();
+                        List<Elder> filteredList = new ArrayList<>();
+
+                        if (constraint == null || constraint.length() == 0) {
+                            // If the search bar is empty, show the original data
+                            filteredList.addAll(elderlies);
+                        } else {
+                            // If there's a query, apply custom filtering for partial matches
+                            String filterPattern = constraint.toString().toLowerCase().trim();
+                            for (Elder elder : elderlies) {
+                                if (elder.getID().toLowerCase().contains(filterPattern)) {
+                                    filteredList.add(elder);
+                                }
+                            }
+                        }
+
+                        results.values = filteredList;
+                        results.count = filteredList.size();
+                        return results;
+                    }
+
+                    @Override
+                    protected void publishResults(CharSequence constraint, FilterResults results) {
+                        clear();
+                        addAll((List<Elder>) results.values);
+                        notifyDataSetChanged();
+                    }
+                };
+                return filter;
+            }
         };
         elderliesList.setAdapter(elderAdapter);
-    }
-}
 
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.isEmpty()) {
+                    // If the search bar is empty, refresh the list by setting the original data
+                    elderAdapter.clear();
+                    elderAdapter.addAll(elderlies);
+                    elderAdapter.notifyDataSetChanged();
+                } else {
+                    // If there's a query, apply filtering
+                    elderAdapter.getFilter().filter(newText);
+                }
+                return false;
+            }
+        });
+        // Set the OnCloseListener on the SearchView
+        searchBar.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                // If the user clicks the close button (X), show all elderlies
+                elderAdapter.clear();
+                elderAdapter.addAll(elderlies);
+                elderAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+
+
+    }
+
+}
